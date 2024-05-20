69ff48a09010a4a8b93fb261273488a980c95982
EzequielJesusHerreraNegrin <usr3763@salesianos-lacuesta.net>
2024-05-20 11:52:06 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header/Header'
+
+function App() {
+  /* const [count, setCount] = useState(0) */
+
+  return (
+    <>
+      <header>
+        <Header />
+      </header>
+      <main></main>
+      <footer></footer>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import style from "./src/components/Header/Header.css"
+
+const Header = () => {
+  return (
+    <p className={style.header}>Administrador de Pacientes de Veterinaria</p>
+  )
+}
+
+export default Header
